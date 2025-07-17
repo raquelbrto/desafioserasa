3f43ce1bd008058c8a3c7386fa24af640161760e
Raquel Brito <raquelbrito662@gmail.com>
2025-07-16 21:34:21 -0300

    feat: create cache config

---
+++ src/main/java/com/dev/brito/desafioserasa/config/CacheConfig.java
@@ -0,0 +1,23 @@
+package com.dev.brito.desafioserasa.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
+    }
+}
