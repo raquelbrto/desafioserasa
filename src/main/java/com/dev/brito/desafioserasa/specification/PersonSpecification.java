caa88972fe6f8ed3c2914216bfc6169dd84907db
Raquel Brito <raquelbrito662@gmail.com>
2025-07-17 18:35:28 -0300

    refactor: create specification

---
+++ src/main/java/com/dev/brito/desafioserasa/specification/PersonSpecification.java
@@ -0,0 +1,45 @@
+package com.dev.brito.desafioserasa.specification;
+
+import com.dev.brito.desafioserasa.entity.Person;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+
+public class PersonSpecification {
+    public static Specification<Person> build(String name, Integer age, String zipCode) {
+        Specification<Person> spec = (root, query, builder) -> null;
+        spec = spec.and(activeTrue());
+
+        if (StringUtils.hasText(name)) {
+            spec = spec.and(nameContains(name));
+        }
+
+        if (age != null) {
+            spec = spec.and(ageEquals(age));
+        }
+
+        if (StringUtils.hasText(zipCode)) {
+            spec = spec.and(zipCodeEquals(zipCode));
+        }
+
+        return spec;
+    }
+
+    public static Specification<Person> activeTrue() {
+        return (root, query, builder) -> builder.isTrue(root.get("active"));
+    }
+
+    public static Specification<Person> nameContains(String name) {
+        return (root, query, builder) ->
+                builder.like(builder.lower(root.get("name")), "%" + name.toLowerCase() + "%");
+    }
+
+    public static Specification<Person> ageEquals(Integer age) {
+        return (root, query, builder) ->
+                builder.equal(root.get("age"), age);
+    }
+
+    public static Specification<Person> zipCodeEquals(String zipCode) {
+        return (root, query, builder) ->
+                builder.equal(root.get("zipCode"), zipCode);
+    }
+}
